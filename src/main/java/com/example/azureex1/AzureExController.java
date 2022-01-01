d44217ed57b6299ee07c95f3d7d7fc8838d9ce84
tamar peled <tamarpe@gmail.com>
2022-01-01 21:34:06 +0100

    Tiny java app

---
+++ src/main/java/com/example/azureex1/AzureExController.java
@@ -0,0 +1,18 @@
+package com.example.azureex1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureExController {
+
+    @RequestMapping
+    public String home() {
+        return "Testing with Azure";
+    }
+
+    @RequestMapping("/page")
+     public String goodbye() {
+        return "Another page";
+    }
+}
---
+++ src/main/java/com/example/azureex1/Azureex1Application.java
@@ -0,0 +1,13 @@
+package com.example.azureex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Azureex1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Azureex1Application.class, args);
+    }
+
+}
